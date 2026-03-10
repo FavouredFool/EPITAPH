e43cbda194e82e475bfbf29623fab7a008d2db90
Lukas Eckart <xahutek11@gmail.com>
2026-03-10 18:11:18 +0100

    Menu

---
+++ EPITAPH/Assets/!!Main/UserInterface/Scripts/Menu/MenuScreen.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using TMPro;
+using DG.Tweening;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class MenuScreen : MonoBehaviour
+{
+    [SerializeField] List<Button> _buttons;
+
+    public void Toggle(bool on)
+    {
+        gameObject.SetActive(on);
+        
+        if (on)
+        {
+            _buttons[0].Select();
+        }
+    }
+}
