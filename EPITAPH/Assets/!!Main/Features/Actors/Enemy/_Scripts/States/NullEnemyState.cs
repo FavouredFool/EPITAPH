35ec970fbac5973001cd4df3a08a4e05ba0b3107
Tim Wahrburg <t.wahrburg@gmail.com>
2026-03-13 17:49:39 +0100

    null fix movement

--- EPITAPH/Assets/!!Main/Features/Actors/Enemy/_Scripts/States/NullEnemyState.cs
+++ EPITAPH/Assets/!!Main/Features/Actors/Enemy/_Scripts/States/NullEnemyState.cs
@@ -4,5 +4,13 @@ public class NullEnemyState : EnemyBaseState
 {
     public NullEnemyState(EnemyStateContext ctx) : base(ctx)
     {
+        
     }
+    
+
+    public override void OnEnter()
+    {
+        _ctx.EnemyController.Rb.linearVelocity = Vector2.zero;
+    }
+
 }
