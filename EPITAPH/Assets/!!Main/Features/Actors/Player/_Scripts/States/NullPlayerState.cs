bc46717bb5873139add0a003a9be2d7997725535
Tim Wahrburg <t.wahrburg@gmail.com>
2026-03-13 17:53:10 +0100

    null update

--- EPITAPH/Assets/!!Main/Features/Actors/Player/_Scripts/States/NullPlayerState.cs
+++ EPITAPH/Assets/!!Main/Features/Actors/Player/_Scripts/States/NullPlayerState.cs
@@ -5,4 +5,9 @@ public class NullPlayerState : VampireBaseState
     public NullPlayerState(VampireStateContext ctx) : base(ctx)
     {
     }
+
+    public override void OnEnter()
+    {
+        _ctx.PlayerController.Rb.linearVelocity = Vector2.zero;
+    }
 }
